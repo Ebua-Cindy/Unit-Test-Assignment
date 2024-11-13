2dffca6f2e865a6d4fe0b1334eba8ec0cb8f5893
Ebua Cindy <ebuacindy@gmail.com>
2024-11-13 16:56:08 +0100

    Error 500

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/project_route')
+@app.route('/')
 def home():
-    return jsonify(message="Hello level 400 FET, Quality Assurance!")
+    return jsoniy(message="Hello level 400 FET, Quality Assurance!")
 
 
 if __name__ == '__main__':
